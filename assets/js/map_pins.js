bc85d85ae5d90bf5e8ec03ccdbb3ce602d2f2ddd
RitikaHiremath <ritika.hiremath@gmail.com>
2025-01-10 15:22:51 +0100

    floating button added

--- assets/js/map_pins.js
+++ assets/js/map_pins.js
@@ -117,3 +117,24 @@ function moveTo(name){
         console.error(`Element with ID "${name}" not found.`);
     }
 }
+// Get the button:
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    // mybutton.style.position = "absolute";
+  } else {
+    mybutton.style.display = "none";
+    // mybutton.style.position = "fixed";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
