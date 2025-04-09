d0f56b6f5e6b8c57cbf280972be777b30cc91190
RitikaHiremath <ritika.hiremath@gmail.com>
2025-04-09 15:27:59 +0200

    map responsiveness and up arrow fixes

--- assets/js/map_pins.js
+++ assets/js/map_pins.js
@@ -46,10 +46,22 @@ const mapBounds = {
     bottomRight: { lat: 47.40724, lon: 14.98853 }
 };
 
-const svgDimensions = {
-    width: 600,
-    height: 800
-};
+// const svgDimensions = {
+//     width: 600,
+//     height: 800
+// };
+function getSvgSize() {
+    const svg = document.getElementById("germany-map");
+    const viewBox = svg.viewBox.baseVal;
+    const boundingBox = svg.getBoundingClientRect();
+    return {
+        width: boundingBox.width,
+        height: boundingBox.height,
+        viewBoxWidth: viewBox.width,
+        viewBoxHeight: viewBox.height
+    };
+}
+
 
 function addPin(uni) {
     const coords = convertLatLonToXY(uni.lat, uni.lon);
@@ -118,12 +130,17 @@ function addPin(uni) {
 }
 
 
+// function convertLatLonToXY(lat, lon) {
+//     const x = ((lon - mapBounds.topLeft.lon) / (mapBounds.bottomRight.lon - mapBounds.topLeft.lon)) * svgDimensions.width;
+//     const y = ((mapBounds.topLeft.lat - lat) / (mapBounds.topLeft.lat - mapBounds.bottomRight.lat)) * svgDimensions.height;
+//     return { x, y };
+// }
 function convertLatLonToXY(lat, lon) {
-    const x = ((lon - mapBounds.topLeft.lon) / (mapBounds.bottomRight.lon - mapBounds.topLeft.lon)) * svgDimensions.width;
-    const y = ((mapBounds.topLeft.lat - lat) / (mapBounds.topLeft.lat - mapBounds.bottomRight.lat)) * svgDimensions.height;
+    const { viewBoxWidth, viewBoxHeight } = getSvgSize();
+    const x = ((lon - mapBounds.topLeft.lon) / (mapBounds.bottomRight.lon - mapBounds.topLeft.lon)) * viewBoxWidth;
+    const y = ((mapBounds.topLeft.lat - lat) / (mapBounds.topLeft.lat - mapBounds.bottomRight.lat)) * viewBoxHeight;
     return { x, y };
 }
-
 
 function createPinImage() {
     const xmlns = "http://www.w3.org/2000/svg";
@@ -188,28 +205,6 @@ function resetPin(uniName) {
         label.style.display = "none"; // Hide university name
     }
 }
-
-// // Get the button:
-// let mybutton = document.getElementById("topBtn");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//     // mybutton.style.position = "absolute";
-//   } else {
-//     mybutton.style.display = "none";
-//     // mybutton.style.position = "fixed";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
 
 function readMore(link){
     let summary = link.parentNode.querySelector(".summary");
